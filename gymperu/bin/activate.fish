9ab86639da4b85e824585a243ea56928d7dd928b
joelengt <joelengt@gmail.com>
2016-04-19 19:01:55 -0500

    arreglando rutas

--- gymperu/bin/activate.fish
+++ gymperu/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/root/gymperu/gymperu"
+set -gx VIRTUAL_ENV "/home/baudelaire/Escritorio/newgym/gymperu"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
