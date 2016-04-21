c2d39be1904b188f1dd314470835cf5e0998bcfd
Maycon <rx_reycel96@hotmail.com>
2016-04-21 00:18:16 -0500

    se agregaron estilos

---
+++ static/js/app.js
@@ -0,0 +1,31 @@
+$(function(){
+  $("#apple-crsl .b_indicators .carousel-inner").slidesjs({
+    width: 940,
+    height: 350,
+    navigation: {
+      active: false
+    },
+    play: {
+      active: false,
+      auto: true,
+      effect: 'slide',
+      interval: 3000,
+    }
+  });
+
+  $('.effect').viewportChecker({
+    classToAdd: 'moveAnimation',
+    offset: 150
+  });
+
+  $('.btn-up').click(function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: 0
+    }, 1000); 
+  });
+
+
+}); 
+
+    
